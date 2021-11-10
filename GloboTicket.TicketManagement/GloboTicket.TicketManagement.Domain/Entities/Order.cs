4d0ce0497805f41947dff46e92289e09f9fd617a
Katarina Ivosevic <k.ivosevic@vega.local>
2021-11-10 17:13:52 +0100

    Added entities to the Domain project

---
+++ GloboTicket.TicketManagement/GloboTicket.TicketManagement.Domain/Entities/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GloboTicket.TicketManagement.Domain.Entities
+{
+    public class Order
+    {
+        public Guid OrderId { get; set; }
+        public Guid UserId { get; set; }
+        public int OrderTotal { get; set; }
+        public DateTime OrderPlaced { get; set; }
+        public bool OrderPaid { get; set; }
+    }
+}
