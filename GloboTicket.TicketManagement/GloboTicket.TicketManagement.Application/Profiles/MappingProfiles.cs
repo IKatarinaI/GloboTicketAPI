a459027b07ec24b2e16bf65eb4edf70a0826f3a1
Katarina Ivosevic <katarinaivosevic1995@outlook.com>
2021-11-27 21:29:53 +0100

    Added EventHandler and models for Mediatr

---
+++ GloboTicket.TicketManagement/GloboTicket.TicketManagement.Application/Profiles/MappingProfiles.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Features.Events;
+using GloboTicket.TicketManagement.Domain.Entities;
+
+namespace GloboTicket.TicketManagement.Application.Profiles
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Event, EventListVM>().ReverseMap();
+        }
+    }
+}
